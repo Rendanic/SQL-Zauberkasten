3ef7d0cc16e608db908ca6d2bf886fa440aac4d0
oraculix <uwe@kuechler.org>
2015-01-29 14:12:42 +0100

    Update audi.sql

--- sql/audit/audi.sql
+++ sql/audit/audi.sql
@@ -1,3 +1,4 @@
+REM --------------------------------------
 REM Auditing Overview
 REM --------------------------------------
 REM Uwe Küchler (uwe.kuechler@opitz-consulting.de)
@@ -8,6 +9,8 @@ set lines 155 pages 9999
 set feedback off
 
 prompt
+prompt
+prompt ================
 prompt Audit Parameters
 prompt ================
 
@@ -20,9 +23,11 @@ select NAME, VALUE, DESCRIPTION
  order by NAME;
 
 prompt
+prompt
+prompt ============
 prompt Audit Tables
 prompt ============
- 
+
 SELECT t.table_name, t.tablespace_name, ROUND( t.blocks * s.block_size / 1024 / 1024, 2 ) MB
   FROM dba_tables t, dba_tablespaces s
  WHERE t.table_name IN ('AUD$', 'FGA_LOG$')
@@ -32,9 +37,10 @@ SELECT t.table_name, t.tablespace_name, ROUND( t.blocks * s.block_size / 1024 / 
 
 prompt
 prompt
+prompt =======================
 prompt Statement Audit Options
 prompt =======================
- 
+
 col USER_NAME        for a15
 col PROXY_NAME       for a15
 
@@ -45,9 +51,10 @@ select *
 
 prompt
 prompt
+prompt =======================
 prompt Privilege Audit Options
 prompt =======================
- 
+
 col USER_NAME        for a15
 col PROXY_NAME       for a15
 
@@ -55,11 +62,12 @@ select *
   from DBA_PRIV_AUDIT_OPTS
  order by USER_NAME nulls first, PROXY_NAME nulls first, PRIVILEGE;
 
- 
+
 prompt
 prompt
-Prompt Audit Management Options
-Prompt ========================
+prompt ========================
+prompt Audit Management Options
+prompt ========================
 
 col PARAMETER_NAME    for a30
 col PARAMETER_VALUE   for a20
@@ -68,3 +76,14 @@ col AUDIT_TRAIL       for a20
 select *
   from DBA_AUDIT_MGMT_CONFIG_PARAMS
  order by AUDIT_TRAIL, PARAMETER_NAME;
+
+
+prompt
+prompt
+prompt =======================
+prompt Last Archival Timestamp
+prompt =======================
+
+SELECT * FROM dba_audit_mgmt_last_arch_ts;
+
+prompt
