b5c3feb3dcd630f4eabac58b229a8c314db7bb51
Thorsten Bruhns <thorsten.bruhns@opitz-consulting.com>
2015-09-26 07:14:13 +0200

    added commit for do not use trigger_open_all.sql in 12.1.0.2 or newer

--- sql/pdb/trigger_open_all.sql
+++ sql/pdb/trigger_open_all.sql
@@ -1,10 +1,15 @@
 
 -- Thorsten Bruhns (Thorsten.Bruhns@opitz-consulting.de)
 --
--- Version: 1
--- Date   : 25.11.2013
+-- Version: 2
+-- Date   : 36.09.2015
 --
 -- Create Trigger for starting all pluggable databases
+--
+-- This is only needed for 12.1.0.1. Please do not use it in
+-- 12.1.0.2 or newer as these versions has an enhancement SQL
+-- syntax for enable/disable autostart of PDBs while starting
+-- the instance
 --
 CREATE OR REPLACE TRIGGER OPEN_ALL_PDBS
 after startup on database
