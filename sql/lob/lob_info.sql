c96932351e39d5ac3a743d249b709ad4ee6ff055
Thorsten Bruhns <thorsten.bruhns@opitz-consulting.com>
2016-02-11 09:41:11 +0100

    added tablespace_name in output

--- sql/lob/lob_info.sql
+++ sql/lob/lob_info.sql
@@ -1,9 +1,11 @@
 --
 -- Thorsten Bruhns (Thorsten.Bruhns@opitz-consulting.de)
--- $Id: lob_info.sql 85 2010-03-23 20:11:44Z tbr $
 --
 -- Displays infos for given LOB-Segment
-set lines 120
+--
+-- Date 11.02.2016
+--
+set lines 140
 set pages 100
 set verify off
 
@@ -12,6 +14,7 @@ column TABLE_NAME format a30
 column owner format a30
 column column_name format a30
 column cache format a5
+column tablespace_name format a30
 
 
 select owner
@@ -19,6 +22,7 @@ select owner
       ,column_name
       ,in_row
       ,cache
+      ,tablespace_name
 from dba_lobs a
 where  a.owner like ('%&1')
   and a.table_name like ('&2')
