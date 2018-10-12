b0fb2385ef212e9606a7acf8fac76d1287804e1d
Bruhns, Thorsten <Thorsten.Bruhns@opitz-consulting.com>
2018-10-12 21:11:38 +0200

    vpdb.sql: added GUID

--- sql/pdb/vpdb.sql
+++ sql/pdb/vpdb.sql
@@ -1,8 +1,8 @@
 --
 -- Thorsten Bruhns (Thorsten.Bruhns@opitz-consulting.de)
 --
--- Version: 1
--- Date   : 25.11.2013
+-- Version: 2
+-- Date   : 12.10.2018
 --
 -- Display information from v$pdbs
 --
@@ -12,18 +12,20 @@ set pages 100
 set verify off
 
 
-column NAME format a40
+column NAME format a20
 column DBID format 999999999999
+column GUID format a40
 column status format a13
 
 select NAME
+      ,GUID
       ,DBID
       ,OPEN_MODE
       ,restricted
       ,RECOVERY_STATUS RECOVERY
       ,sum(total_size)/1024/1024 ts_MB
   from v$pdbs
-  group by grouping sets((name, dbid, open_mode, restricted, RECOVERY_STATUS)
+  group by grouping sets((name, dbid, guid, open_mode, restricted, RECOVERY_STATUS)
                         ,()
                         )
 order by 1;
