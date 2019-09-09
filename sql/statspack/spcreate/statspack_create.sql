8242d0248e709e13252030f49b490e93e8036953
Oraculix <uwe@kuechler.org>
2019-09-09 20:37:24 +0200

    Statspack: Switch from deprecated DBMS_JOB to Scheduler

--- sql/statspack/spcreate/statspack_create.sql
+++ sql/statspack/spcreate/statspack_create.sql
@@ -2,9 +2,14 @@
 -- Thorsten Bruhns (Thorsten.Bruhns@opitz-consulting.de)
 -- $Id: statspack_create.sql 55 2010-03-13 09:18:15Z tbr $
 --
--- This script creates the statspack-Schema
--- It must be run on the database-server
+-- --------------------------------------------------------------------------
+-- This script creates the Statspack Schema
+-- It must be run on the database server AS SYSDBA
+-- *********** Runs on Oracle >= 11.2 ************
+-- For Oracle < 11.2 install necessary jobs with statspack_job.sql
+-- --------------------------------------------------------------------------
 --
+-- 2019-09-09 U. Kuechler: Use Scheduler instead of DBMS_JOB
 -- 2015-09-03 U. Kuechler: Re-fill STATS$IDLE_EVENT with latest idle events
 -- 2016-06-23 U. Kuechler: Check for missing column bug in 12.1
 -- 2015-09-03 U. Kuechler: Jobnr. ausgeben; doppeltes SQL entfernt.
@@ -49,26 +54,49 @@ end;
 PROMPT Setting parameters and taking a first snapshot...
 exec statspack.snap(i_snap_level => &snaplevel, i_modify_parameter => 'TRUE')
 
-PROMPT Creating Jobs for Snapshot-Creation. Must be done with dbms_job due to instance
+PROMPT Creating Jobs for Snapshot-Creation. Must be done with instance
 PROMPT stickyness for RAC-Databases!
 
-set serveroutput on
-declare 
-jobno number;
+declare
+  v_job_name VARCHAR2(30);
+  instcnt PLS_INTEGER;
 begin
-  for cur_inst in(
-  select instance_number from gv$instance
-  )
-  loop
-     dbms_job.submit(jobno
-               , 'begin execute immediate ''alter session set "_cursor_plan_unparse_enabled" = false''; statspack.snap; end;'
-               , trunc(sysdate+1/24,'HH')
-               , &snapinterval
-               , TRUE
-               , cur_inst.instance_number);
-     commit;
-     dbms_output.put_line('Job #'||jobno||' created.');
-  end loop;
+  select count(*) into instcnt from gv$instance;
+  if instcnt > 1 then -- -------------------------------- RAC
+    for cur_inst in(
+      select instance_number from gv$instance
+    )
+    loop
+      v_job_name := 'STATSPACK_SNAP_'||cur_inst.instance_number;
+      dbms_scheduler.create_job( job_name => v_job_name
+        , job_type => 'STORED_PROCEDURE'
+        , job_action => '"PERFSTAT"."STATSPACK"."SNAP"'
+        , repeat_interval => 'FREQ=HOURLY;BYTIME=5500'
+        --, start_date => to_timestamp_tz('2019-09-06 17:30:00 Europe/Berlin', 'YYYY-MM-DD HH24:MI:SS TZR')
+        , comments => 'Take Statspack snapshot'
+        , auto_drop => FALSE
+        , enabled => FALSE);
+      SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
+        ( name      => v_job_name
+         ,attribute => 'INSTANCE_ID'
+         ,value     => TO_NUMBER(cur_inst.instance_number));
+      SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
+        ( name      => v_job_name
+         ,attribute => 'INSTANCE_STICKINESS'
+         ,value     => TRUE);
+      SYS.DBMS_SCHEDULER.ENABLE
+        ( name      => v_job_name);
+    end loop;
+  else -- -------------------------------- Single Instance
+      dbms_scheduler.create_job( job_name => 'STATSPACK_SNAP'
+        , job_type => 'STORED_PROCEDURE'
+        , job_action => '"PERFSTAT"."STATSPACK"."SNAP"'
+        , repeat_interval => 'FREQ=HOURLY;BYTIME=5500'
+        --, start_date => to_timestamp_tz('2019-09-06 17:30:00 Europe/Berlin', 'YYYY-MM-DD HH24:MI:SS TZR')
+        , comments => 'Take Statspack snapshot'
+        , auto_drop => FALSE
+        , enabled => TRUE);
+  end if;
 end;
 /
 
@@ -81,7 +109,7 @@ column ins format 999
 
 select * from stats$statspack_parameter;
 show parameter job_queue_processes
-select job,what,interval,instance ins from all_jobs;
+SELECT job_name, last_start_date, next_run_date, enabled FROM user_scheduler_jobs;
 
 -- Re-fill STATS$IDLE_EVENT with latest idle events that Oracle
 -- regularly forgets to update.
@@ -101,4 +129,4 @@ SELECT 'Missing Column "TIMESTAMP" in table "STATS$SQL_PLAN"!'||CHR(10)||
        as WARNING
   FROM c, I
  WHERE c.nt = 0
-   AND I.VERSION BETWEEN '12.1' AND '12.2';
+   AND I.VERSION BETWEEN '12.1' AND '20';
