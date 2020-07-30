4fe8954361dacb31f6779d95106596af07411d6e
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2020-07-30 19:18:31 +0000

    registry.sql: better formatting

--- sql/database/registry.sql
+++ sql/database/registry.sql
@@ -1,21 +1,21 @@
 --
 -- Thorsten Bruhns (Thorsten.Bruhns@opitz-consulting.de)
--- $Id: registry.sql 60 2010-03-13 11:20:37Z tbr $
+--
+-- Date: 30.07.2020
 --
 -- Information for changes in dba_registry_history
 --
 set pages 100
-set lines 100
+set lines 120
 
-column zeit format a17
+column zeit format a14
 column ACTION format a10
 column NAMESPACE format a10
-column VERSION format a12
-column comments format a30
+column VERSION format a22
+column comments format a50
 
 select to_char(ACTION_TIME, 'dd.mm.yy HH24:MI') zeit
       ,ACTION
-      ,NAMESPACE
       ,VERSION
       ,comments
 from DBA_REGISTRY_HISTORY
