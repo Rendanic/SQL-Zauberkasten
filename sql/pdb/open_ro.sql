8aebc1a7e47ed3ace52a79218759a33510903a3e
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2021-05-16 08:49:41 +0000

    pdb: some more helper for pdb wrk

---
+++ sql/pdb/open_ro.sql
@@ -0,0 +1,12 @@
+--
+-- Thorsten Bruhns (Thorsten.Bruhns@googlemail.com)
+--
+-- Version: 1
+-- Date   : 15.05.2021
+--
+-- Command: alter pluggable database open read only;
+--
+-- Parameter 1: PDB-Name
+
+PROMPT alter pluggable database &1 open read only
+alter pluggable database &1 open read only;
